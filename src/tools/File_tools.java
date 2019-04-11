51e76ebc5d91afad63fbb2c747c6e8754afd3db5
Unknown <andreluis2006@outlook.com>
2019-04-11 17:10:50 -0300

    Adicionado atributo contendo caminho do arquivo de extrato e de contas.

--- src/tools/File_tools.java
+++ src/tools/File_tools.java
@@ -1,14 +1,17 @@
 package tools;
 
-import entity.Account;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+
 public class File_tools {
+
+    private static String account_path_file = "C:\\Users\\dante\\Downloads\\contas.txt";
+    private static String statement_path_file = "C:\\Users\\dante\\Downloads\\extratos.txt";
+
     public long file_size() {
         long aux = 0;
         try {
@@ -27,23 +30,23 @@ public class File_tools {
     }
 
     public static File accounts_file(){
-        File file = new File("C:\\Users\\dante\\Downloads\\contas.txt");
+        File file = new File(account_path_file);
         return  file;
     }
 
 
     public static File statement_file(){
-        File file = new File("C:\\Users\\dante\\Downloads\\extratos.txt");
+        File file = new File(statement_path_file);
         return file;
     }
 
     public static Path statement_path(){
-        Path path = Paths.get("C:\\Users\\dante\\Downloads\\extratos.txt");
+        Path path = Paths.get(statement_path_file);
         return path;
     }
 
     public static Path accounts_path(){
-        Path path = Paths.get("C:\\Users\\dante\\Downloads\\contas.txt");
+        Path path = Paths.get(account_path_file);
         return path;
 
     }
